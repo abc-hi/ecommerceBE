4294489738202df22dfdaa0da1ce922e86f7eee2
abc-hi <revathimohancse@gmail.com>
2024-09-26 01:05:48 -0700

    frontend backend connected

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ import deleterouter from './Routes/deleteRoute.js'
 
 const app= express();
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://revaecommerce.netlify.app',
       methods: ['GET', 'POST', 'PUT', 'DELETE'],  // Define allowed methods
     credentials: true,  // Include credentials like cookies if needed
 }));
